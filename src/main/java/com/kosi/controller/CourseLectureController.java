bd52d82313f6cd786753f680f574978b689cc848
seonhakji <jshag1990@gmail.com>
2025-03-28 12:57:58 +0900

    [Feat] 집체교육 페이지 추가

--- src/main/java/com/kosi/controller/CourseLectureController.java
+++ src/main/java/com/kosi/controller/CourseLectureController.java
@@ -8,6 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/course-lecture")
 public class CourseLectureController {
 
+    //집체교육 소개
+    @GetMapping("/group-education/introduction")
+    public String groupEducationIntroductionPage(){
+        return "/course_lecture/group_education/introduction";
+    }
+
     @GetMapping("/course/save")
     public String courseSavePage(){
         return "/mypage/course/save";
